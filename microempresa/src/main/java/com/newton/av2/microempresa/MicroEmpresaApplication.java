e2280c2d17a3869829564632964158376bc6312d
DanielRabeloChaves <danielrabelochaves1999@gmail.com>
2021-12-06 20:34:48 -0300

    av2

---
+++ microempresa/src/main/java/com/newton/av2/microempresa/MicroEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.newton.av2.microempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEmpresaApplication.class, args);
+	}
+
+}
---
+++ microempresa/src/test/java/com/newton/av2/microempresa/MicroEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newton.av2.microempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
